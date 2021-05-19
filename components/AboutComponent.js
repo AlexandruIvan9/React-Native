528f72f91b74215270f111795f2e37414fec3309
Alex <alexandruivan1995@gmail.com>
2021-05-19 12:40:13 -0500

    Week1

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
-import { ScrollView, Text, FlatList } from 'react-native';
-import { Card, ListItem } from 'react-native-elements';
-import { connect } from 'react-redux';
-import { baseUrl } from '../shared/baseUrl';
-import Loading from './LoadingComponent';
+import { ScrollView} from 'react-native-gesture-handler';
+import { Text } from 'react-native';
+import { Card } from 'react-native-elements';
+import { FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import { PARTNERS } from '../shared/partners';
 
-const mapStateToProps = state => {
-    return {
-        partners: state.partners
-    };
-};
+// ----- MISSION FUNCTIONAL COMPONENT ----- //
+// Used on line 45, below 👇
+const Mission = () => {
+    return (
+        <Card title="Our Mission" wrapperStyle={{margin: 20}}>
+            <Text>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.</Text>
+        </Card>
+    )
+}
+// ----- ----- ----- ----- ----- ----- ----- //
 
 class About extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            partners: PARTNERS
+        };
+    }
     
     static navigationOptions = {
         title: "About Us"
@@ -23,50 +35,25 @@ class About extends Component {
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{source: {uri: baseUrl + item.image}}}
+                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
                 />
             );
         };
-
-        if (this.props.partners.isLoading) {
-            return (
-                <ScrollView>
-                    <Mission />
-                    <Card
-                        title='Community Partners'>
-                        <Loading />
-                    </Card>
-                </ScrollView>
-            );
-        }
-        if (this.props.partners.errMess) {
-            return (
-                <ScrollView>
-                    <Mission />
-                    <Card
-                        title='Community Partners'>
-                        <Text>{this.props.partners.errMess}</Text>
-                    </Card>
-                </ScrollView>
-            );
-        }
         return (
             <ScrollView>
-                <Card title="Our Mission" wrapperStyle={{margin: 20}}>
-                    <Text>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.</Text>
-                </Card>
-                
+                <Mission/>
                 <Card title="Community Partners " wrapperStyle={{margin: 20}}>
-                <FlatList 
-                        data={this.props.partners.partners}
+                    <FlatList
+                        data={this.state.partners}
                         renderItem={renderPartner}
                         keyExtractor={item => item.id.toString()}
-                />
+                    />
                 </Card>
+
             </ScrollView>
         );
     }
 }
 
-export default connect(mapStateToProps)(About);
+export default About;
 
